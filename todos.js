c80bb573f26fa46a61513bc7d2987bf8668f7a8f
lindamariemummy <lindamariemummy@gmail.com>
2014-09-18 15:06:14 -0700

    rearranged for clarity

--- todos.js
+++ todos.js
@@ -10,12 +10,11 @@ $(document).ready(function() {
 			$(".tableOfTodos").append("<tr><td><div class='todobox'>" + $entry + "</div></td></tr>");					
 			$("#userEntry").val(""); //resets textbox
 			$("#error").text(""); //blanks out error message
-		}	  
-		
+		}		
 		$('.todobox').on("click", function() { //todo list items disappear when clicked
 			$(this).fadeOut("fast");		
-		});
-
+		});  
 	});	
+
 });
 
